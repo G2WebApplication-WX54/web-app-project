c1e6796dd519bd755fb40147f7c3f98e26a78d87
ern23zc <U202112907@upc.edu.pe>
2024-05-04 08:58:22 -0500

    feat Principal
    Principal page has been created

---
+++ src/pages/principal.page.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <img class="logo" src="/src/assets/PeaceApp.png" alt="logo" />
+    <h2>For more info, check out our Landing Page and discover what we offer</h2>
+    <router-link to="https://g2webapplication-wx54.github.io/landing-page-web-app/">
+      <button>More Information</button>
+    </router-link>
+  </div>
+  <div class="container2">
+
+  </div>
+</template>
+
+<style scoped>
+button {
+  background-color: #EEF221;
+  color: #161616;
+  font-weight: bolder;
+  border: none;
+  border-radius: 0.25em;
+  padding: 0.5em 1em;
+  margin: 0.5em;
+  cursor: pointer;
+  font-size: 20px;
+  height: 45px;
+  width: 30%;
+}
+</style>
