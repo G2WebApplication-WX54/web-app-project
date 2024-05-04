0c62de03d6a58c865e3dff769f04fd72d4a5c20b
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-05-04 01:30:38 -0500

    feat: all without reports in the order list

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<script >
+import ReportListComponent from "./reports/report-list.component.vue";
+export default{
+  name: "app",
+  components: {ReportListComponent}
+}
+</script>
+
+<template>
+<ReportListComponent/>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from './app.vue'
+import PrimeVue from "primevue/config";
+import "primeflex/primeflex.css";
+import OrderList from "primevue/orderlist";
+import Panel from "primevue/panel";
+import Button from "primevue/button";
+import Card from "primevue/card";
+import 'primevue/resources/themes/aura-light-green/theme.css';
 
-createApp(App).mount('#app')
+const app=createApp(App)
+app.use(PrimeVue, {ripple: true})
+    .component('pv-button', Button)
+    .component('pv-panel', Panel)
+    .component('pv-data-view', DataView)
+    .component('pv-card', Card)
+    .component('pv-order-list', OrderList)
+app.mount('#app')
