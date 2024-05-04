3a5ae4a786cef907302db5756cf42d44a5244e42
ern23zc <U202112907@upc.edu.pe>
2024-05-04 10:00:27 -0500

    Update principal.page.vue
    Principal page has been updated

--- src/pages/principal.page.vue
+++ src/pages/principal.page.vue
@@ -1,17 +1,32 @@
-<script setup>
+<script>
+export default {
+  methods: {
+    redirectLanding() {
+      const Link = 'https://g2webapplication-wx54.github.io/landing-page-web-app/';
 
+      window.location.href = Link;
+    }
+  }
+}
 </script>
 
 <template>
   <div class="container">
-    <img class="logo" src="/src/assets/PeaceApp.png" alt="logo" />
-    <h2>For more info, check out our Landing Page and discover what we offer</h2>
-    <router-link to="https://g2webapplication-wx54.github.io/landing-page-web-app/">
-      <button>More Information</button>
-    </router-link>
-  </div>
-  <div class="container2">
-
+    <div class="box1">
+      <img class="logo" src="/src/assets/PeaceApp.png" alt="logo" />
+      <h2 class="info">For more info, check out our Landing Page and discover what we offer</h2>
+      <button @click="redirectLanding">More Information</button>
+    </div>
+    <div class="box2">
+      <h2>Welcome to Peace App</h2>
+      <h3>Please, log in to start</h3>
+      <input type="text" class="textbox" style="width: 80%;" placeholder="E-mail">
+      <input type="text" class="textbox" style="width: 80%;" placeholder="Password">
+      <router-link to="/government/map">
+        <button>Log in</button>
+      </router-link>
+      <h4>Forgot your password? <a href="#">Click here</a></h4>
+    </div>
   </div>
 </template>
 
@@ -27,6 +42,40 @@ button {
   cursor: pointer;
   font-size: 20px;
   height: 45px;
-  width: 30%;
+  width: 40%;
+}
+
+.box2{
+  background-color: #6DC9FF;
+}
+
+.info{
+  padding: 50px;
+}
+.container {
+  display: flex;
+  margin-top: 60px;
+  font-family: 'Montserrat', sans-serif;
+  color: black;
+  padding: 20px;
+  flex-wrap: wrap;
+  height: auto;
+}
+.textbox {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+
+.box1, .box2 {
+  flex: 1;
+  padding: 20px;
+  margin: 10px;
+  width: 100%;
+}
+
+.textbox {
+  width: 100%; /* El ancho del textbox ocupará todo el ancho del contenedor */
+  box-sizing: border-box; /* Incluye el padding y el borde en el ancho total del textbox */
 }
 </style>
