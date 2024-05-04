9dd4f015d55d192e280e081af668025f26ed98b5
ern23zc <112828936+ern23zc@users.noreply.github.com>
2024-05-03 22:00:01 -0500

    feat Toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Toolbar from "./components/toolbar.component.vue"
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Toolbar/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <div class="container">
+      <router-view/>
+    </div>
+  </main>
 </template>
 
 <style scoped>
